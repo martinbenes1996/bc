49801768b9e4aae7c29476b4c0fc2b36c4f9eaef
Martin Benes <xbenes49@stud.fit.vutbr.cz>
2019-01-07 22:11:06 +0100

    show

--- data/show.py
+++ data/show.py
@@ -3,7 +3,9 @@
 import socket
 import sys
 import time
-#import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 if len(sys.argv) == 3 and sys.argv[1] == '-f':
     filename = sys.argv[2]
@@ -11,7 +13,15 @@ else:
     filename = 'file.dat'
 
 f = open(filename, 'rb')
-data = f.read()
+data = f.read() 
 
 l = [int.from_bytes(data[i:i+2],byteorder='big') for i in range(0,len(data)-1,2)]
-print(l)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(l, color='magenta')
+ax.set_xlabel('time')
+ax.set_ylabel('signal')
+
+plt.ylim(0,1027)
+plt.show()
