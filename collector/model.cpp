09066dcb0717744ad11e0d77770c0baf2ae8f9ba
Martin Benes <xbenes49@stud.fit.vutbr.cz>
2019-01-18 11:21:33 +0100

    recognition documented

--- collector/model.cpp
+++ collector/model.cpp
@@ -1,6 +1,60 @@
 
-#include <opencv2/opencv.hpp>
+#include <vector>
 
 #include "model.h"
+
+class Wavelet {
+    public:
+        enum Type {
+            Morlet,
+            Coiflet,
+            Daubechies,
+            Biorthogonal,
+            MexicanHat,
+            Symlet,
+        };
+
+        Wavelet(Type t, int scale): s_(scale) {
+            switch(t) {
+                case Morlet:
+                    f_ = [](double x){return x;};
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        double operator*(const cv::Mat& x) {
+            std::cerr << "Operator * called!\n";
+            return 0.;
+        }
+
+    private:
+        std::function<double(double)> f_ = [](double){return 0.;};
+        unsigned s_;
+
+};
+
+void CWT(cv::Mat x, Wavelet wavelet, std::vector<unsigned> scales, cv::Mat& y) {
+    unsigned N = x.cols;
+    unsigned S = scales.size();
+    cv::Mat features = cv::Mat::zeros(S, N, CV_32F);
+
+
+
+    // iterate over shifts (rows)
+    for(unsigned n = 0; n < N; n++) {
+        // iterate over scales (columns)
+        for(auto& s: scales) {
+
+            // iterate over signal (convolution)
+            for(unsigned t = 0; t < N; t++) {
+                
+                features.at<float>(n,s);
+            }
+
+        }
+    }
+}
 
 
--- collector/model.h
+++ collector/model.h
@@ -6,74 +6,100 @@
 #include <cstdlib>
 #include <functional>
 #include <map>
+#include <memory>
 #include <mutex>
 #include <string>
 #include <time.h>
 #include <vector>
 
+#include <opencv2/opencv.hpp>
+
 #include "globals.h"
 #include "log.h"
 
+/** @brief Recognition classes. */
 namespace Recog {
-    class Exception {
+    
+    /** @brief Exception of Recog components. */
+    class Exception: public std::exception {
         public:
-            Exception(const char *msg) { msg_ = msg; }
+            /**
+             * @brief Constructor.
+             * @param msg       Description of exception.
+             */
+            Exception(const char *msg): msg_(msg) {}
+            /**
+             * @brief Description getter.
+             * @returns Description of exception.
+             */
             std::string what() { return std::string(msg_); }
         private:
-            const char * msg_;
+            const char * msg_; /**< Description */
     };
 
-    typedef Geo::Coords::Polar Object;
-    struct Result {
-        unsigned timestamp;
-        std::vector<Object> data;
 
+    /** @brief Recognized object in polar coordinates. */
+    typedef Geo::Coords::Polar Object;
+
+    /** @brief Features extracted from the signal. */
+    typedef cv::Mat Features;
+    
+    /** @brief Result of recognition (objects) to send. */
+    struct Result {
+        unsigned timestamp; /**< Timestamp of object. */
+        std::vector<Object> data; /**< Recognized objects. */
+
+        /** @brief Formatted print. */
         void log() {
             std::cerr << "Result " << timestamp << ": ";
             for(auto &it: data) {
                 it.log();
                 std::cerr << " ";
             }
-            if(data.size() > 0) std::cerr << "\x08.\n";
+            if(data.size() > 0) std::cerr << "\x08.\n"; // bksp
             else std::cerr << "No objects detected.\n";
         }
 
+        /**
+         * @brief Buffer size getter.
+         * @returns Buffer size in B.
+         */
         unsigned bufferSize() { return (1 + data.size()*2) * sizeof(int); }
 
+        /**
+         * @brief Raw buffer exporter. Used when sending data.
+         *        The data has following format:
+         *          4B object count N, n*(4+4)B object data
+         *          Object data contains azimuth and distance.
+         *        Endianness not solved. 
+         * @returns Buffer address in memory.
+         */
         void * toBuffer() {
-            if(mem != nullptr) { delete[] mem; }
+            // allocate, reallocate if allocated
+            if(mem_ != nullptr) { delete[] mem_; }
             int* buffer = new int [ 1 + data.size()*2 ];
+            // first 4 bytes is count of objects
             buffer[0] = data.size();
+            // pairs of values (azimuth[4B]+distance[4B])  
             for(unsigned i = 0; i < data.size(); i++) {
                 buffer[1 + 2*i] = data[i].azimuth;
                 buffer[1 + 2*i + 1] = data[i].distance;
             }
             return buffer;
         }
-
-        ~Result() { if(mem != nullptr) { std::cerr << "Result::~Result: Deallocating buffer.\n"; delete[] mem; } }
+        /** @brief Destructor. Deallocates the buffer. */
+        ~Result() { if(mem_ != nullptr) { delete[] mem_; } }
 
         private:
-            int * mem = nullptr;
-    };
-
-    struct Features {
-        static const unsigned featN = 13;
-        unsigned timestamp;
-        std::array<int, featN> data;
-
-        bool old() { return (data.size() == 0); }
+            int * mem_ = nullptr; /**< Buffer address. */
     };
 }
 
+/** @brief Hardware abstraction classes. */
 namespace HW {
-    class Exception {
-        public:
-            Exception(const char *msg) { msg_ = msg; }
-            std::string what() { return std::string(msg_); }
-        private:
-            const char * msg_;
-    };
+
+    /** @brief */
+    class Exception: public Recog::Exception {};
 
     /**
      * @brief Abstraction of sensor.
@@ -90,7 +116,7 @@ namespace HW {
             Sensor(std::string name = "",
                    int orientation = 0,
                    Geo::Coords::Polar position = Geo::Coords::Polar()):
-                name_(name), azimuth_(orientation), position_(position) {
+                azimuth_(orientation), position_(position), name_(name) {
                 
                 #ifdef DEBUG_SENSOR
                     DebugPrompt(); std::cerr << "Created. "
@@ -117,6 +143,9 @@ namespace HW {
                 return (featureLock.unlock(), f);
             }
             
+            /**
+             * @brief Actualizes data. Performs feature extraction.
+             */
             void actualizeData(std::array<int, SEGMENT_SIZE> samples) {
                 
                 #ifdef DEBUG_SENSOR
@@ -134,53 +163,64 @@ namespace HW {
                 featureLock.unlock();
             }
 
-
         private:
-            std::string name_;
-            int azimuth_;
-            Geo::Coords::Polar position_;
+            int azimuth_;      /**< Azimuth of the sensor relatively to the global azimuth. */
+            Geo::Coords::Polar position_; /**< Position of the sensor relatively to the center. */
 
-            std::mutex featureLock;
-            Recog::Features features_;
+            std::mutex featureLock; /**< Lock of features for simultaneous access. */
+            Recog::Features features_; /**< Features. */
 
-            static const unsigned featN_ = 13;
-
-            void DebugPrompt() {
-                std::cerr << "Sensor " << name_ << ": ";
-            }
+            std::string name_; /**< Name of the sensor (for logging). */
+            /** @brief Prints out prompt of sensor. */
+            void DebugPrompt() { std::cerr << "Sensor " << name_ << ": "; }
     };
+
 }
 
 namespace Recog {
+
+    /** @brief Fusion performing class. */
     class Fusion {
         public:
+            /** @brief Constructor. */
             Fusion() {
                 const int OMEGA = 50;
                 // central sensor
-                sensors_.emplace( "C", new HW::Sensor("C") );
+                sensors_.emplace( "C", std::make_shared<HW::Sensor>("C") );
                 // left sensor
-                sensors_.emplace("L", new HW::Sensor( "L", // name
-                                                50,  // orientation
-                                                Geo::Coords::Polar ( // position
-                                                    90+((180-OMEGA)/2.), // azimuth
-                                                    sqrt(2*25*25 - 2*25*25*cos(OMEGA/360.*M_PI)) // distance
-                                                )
+                sensors_.emplace("L", std::make_shared<HW::Sensor>(
+                    "L", // name
+                    50,  // orientation
+                    Geo::Coords::Polar ( // position
+                        90+((180-OMEGA)/2.), // azimuth
+                        sqrt(2*25*25 - 2*25*25*cos(OMEGA/360.*M_PI)) // distance
+                    )
                 ));
                 // right sensor
-                sensors_.emplace("R", new HW::Sensor( "R", // name
-                                                -50, // orientation
-                                                Geo::Coords::Polar( // position
-                                                    -90-((180-OMEGA)/2.), // azimuth
-                                                    sqrt(2*25*25 - 2*25*25*cos(-OMEGA/360.*M_PI)) // distance
-                                                )
+                sensors_.emplace("R", std::make_shared<HW::Sensor>(
+                    "R", // name
+                    -50, // orientation
+                    Geo::Coords::Polar( // position
+                        -90-((180-OMEGA)/2.), // azimuth
+                        sqrt(2*25*25 - 2*25*25*cos(-OMEGA/360.*M_PI)) // distance
+                    )
                 ));
             }
-
+            /**
+             * @brief Getter of actualizer of certain sensor.
+             *        Used by Comm, when data arrives.
+             * @param sensorkey     Sensor specifying (L,R,C).
+             * @retuns Function handeling new segment.
+             */
             std::function<void(std::array<int,SEGMENT_SIZE>)> getActualizer(std::string sensorkey) {
-                HW::Sensor* s = sensors_.at(sensorkey);
+                std::shared_ptr<HW::Sensor> s = sensors_.at(sensorkey);
                 return [s](std::array<int,SEGMENT_SIZE> data){ s->actualizeData(data); };
             }
 
+            /**
+             * @brief Fusion algorithm.
+             * @returns Result of the fusion.
+             */
             Result calculate() {
                 // read features from sensors
                 Features fc = sensors_.at("C")->readFeatures();
@@ -195,7 +235,7 @@ namespace Recog {
             }
 
         private:
-            std::map<std::string, HW::Sensor*> sensors_;
+            std::map<std::string, std::shared_ptr<HW::Sensor>> sensors_; /**< Sensor map. */
     };
 }
 
