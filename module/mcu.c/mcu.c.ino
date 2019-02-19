eae39b97c68bcbb2c39b868d3eb511772dcdb999
Martin Benes <martinbenes1996@gmail.com>
2019-02-19 19:33:53 +0100

    module reprogrammed

--- module/mcu.c/mcu.c.ino
+++ module/mcu.c/mcu.c.ino
@@ -1,50 +1,53 @@
-/*  
-    Arduino with PIR motion sensor
-    For complete project details, visit: http://RandomNerdTutorials.com/pirsensor
-    Modified by Rui Santos based on PIR sensor by Limor Fried
-*/
- 
-//int led = 13;                // the pin that the LED is atteched to
-//int sensor = 2;             // sensor 1
-//int Ch1 = A0;
-//int Ch2 = A1;
-//int cnt = 0;
+
+int lastRead;
+int lastSend;
+int mem[60];
+int memIt = 0;
 
 void setup() {
   analogReference(DEFAULT);
-  //pinMode(led, OUTPUT);      // initalize LED as an output
-  //pinMode(sensor, INPUT);   // initialize sensor as an input
   Serial.begin(9600);        // initialize serial
+
+  for(int i = 0; i < 10; i++) {
+    readSample();
+    delay(10);
+  }
+  lastSend = lastRead = millis();
+  memIt = 10;
 }
 
 void loop(){
-  //int val = digitalRead(sensor);
-  // DISCRETE
-  //if(val == HIGH) {
-    //Serial.print("Motion Detected: ");
-    //Serial.println(++cnt);
-    //digitalWrite(led, HIGH);
-    //delay(200);
-    //digitalWrite(led, LOW);
-  //}
-  // continuous
-  /*
-  int val1 = analogRead(Ch1);
-  int val2 = analogRead(Ch2);
-  float v1 = val1*5.0 / 1023;
-  float v2 = val2*5.0 / 1023;
+  int now = millis();
+  if( abs(now-lastRead) > 10 ) {
+    readSample();
+  }
 
-  if(val == LOW) {
-    
-  } else {
-    Serial.print(v1);
-    Serial.print(" ");
-    Serial.println(v2);
-    delay(100);
- }*/
-
- //analogWrite(A7, 255);
- int val = analogRead(A0);
- Serial.println(val);
- delay(10);
+  now = millis();
+  if( abs(now-lastSend) > 500 ) {
+    sendSegment();
+    lastSend = millis();
+  }
+  
 }
+
+void readSample() {
+  if(memIt >= 60) return;
+  mem[memIt++] = analogRead(A0);
+  lastRead = millis();
+}
+
+void sendSegment() {
+  for(int i = memIt; i < 60; i++) {
+    readSample();
+    delay(10);
+  }
+  for(int i = 0; i < memIt; i++) {
+    Serial.println(mem[i]);
+  }
+
+  memcpy(mem, &mem[50], 10*sizeof(int));
+  memIt = 10;
+}
+
+
+
